c2cd19173e36ae2499a016f28f84537c8638ed22
anuhassithijha-tech <anuhassithijha@gmail.com>
2026-05-22 23:18:23 +0530

    Update forward.js

--- plugins/forward.js
+++ plugins/forward.js
@@ -1,44 +1,47 @@
-const config = require('../settings');
-
 module.exports = {
     name: 'forward',
     alias: ['fw'],
     desc: 'Forward replied message',
-    category: 'utility',
-    react: '📨',
+    category: 'owner',
 
     async execute(conn, mek, m, {
-        from,
         reply,
         isOwner
     }) {
+
         try {
 
             if (!isOwner) {
                 return reply('❌ Owner only command');
             }
 
-            // command example
-            // .forward 947XXXXXXXX@s.whatsapp.net
-
-            const jid = m.body.split(' ')[1];
-
-            if (!jid) {
-                return reply('⚠️ Please provide target number\n\nExample:\n.forward 947XXXXXXXX@s.whatsapp.net');
-            }
-
+            // must reply message
             if (!m.quoted) {
                 return reply('⚠️ Reply to a message');
             }
 
-            // Forward message
-            await conn.forwardMessage(jid, m.quoted.message, false);
+            // get jid
+            const args = m.body.split(' ');
+            const jid = args[1];
+
+            if (!jid) {
+                return reply(
+`⚠️ Example:
+
+.forward 94761234567@s.whatsapp.net`
+                );
+            }
+
+            // forward message
+            await conn.sendMessage(jid, {
+                forward: m.quoted
+            });
 
             reply('✅ Message forwarded successfully');
 
         } catch (e) {
             console.log(e);
-            reply('❌ Error while forwarding');
+            reply('❌ Forward failed');
         }
     }
 }
