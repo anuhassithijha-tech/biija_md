2f2ddb71778b0d6073916bd710c93d74c2a3a2c1
anuhassithijha-tech <anuhassithijha@gmail.com>
2026-05-24 12:07:21 +0530

    Update jid.js

--- plugins/jid.js
+++ plugins/jid.js
@@ -2,16 +2,31 @@ const { cmd } = require("../command");
 
 cmd({
     pattern: "jid",
-    desc: "Get Real JID",
+    desc: "Get real jid",
     category: "tools",
     filename: __filename
 },
 async (conn, mek, m, { reply }) => {
 
-    // Group / User real JID
-    let jid = m.chat;
+    let jid;
 
-    // Send real JID only
+    // Reply message user real jid
+    if (m.quoted) {
+        jid = m.quoted.sender;
+    }
+
+    // Group real jid
+    else if (m.isGroup) {
+        jid = m.chat;
+    }
+
+    // Private chat real jid
+    else {
+        jid = m.sender;
+    }
+
+    // Remove LID if exists
+    jid = jid.replace(/@lid/g, "@s.whatsapp.net");
+
     return reply(jid);
-
 });
